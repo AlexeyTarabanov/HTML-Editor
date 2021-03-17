5ddb7c494f848560145be7a2c4ef7a22a54299f3
Alexey Tarabanov <tarabanov@bk.ru>
2021-03-17 21:27:23 +0300

    4/24

--- View.java
+++ View.java
@@ -1,5 +1,7 @@
 package com.javarush.task.task32.task3209;
 
+import com.javarush.task.task32.task3209.listeners.FrameListener;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -29,7 +31,29 @@ public class View extends JFrame implements ActionListener {
     }
 
     // инициализирует
-    public void init() {}
+    public void init() {
+        initGui();
+        FrameListener listener = new FrameListener(this);
+        // добавляем подписчика (регистрации слушателя)
+        addWindowListener(listener);
+        setVisible(true);
+    }
+
+    // инициализация меню
+    public void initMenuBar() {}
+
+    // инициализация панелей редактора
+    public void initEditor() {}
+
+    // инициализация графического интерфейса
+    public void initGui() {
+        initMenuBar();
+        initEditor();
+        // pack() - устанавливает такой минимальный размер контейнера, который достаточен для отображения всех компонентов
+        // должен вызываться после добавления всех элементов, ибо, при расчёте размера окна,
+        // он учитывает только ранее добавленные элементы а элементы добавленные позднее в расчёт не идут.
+        pack();
+    }
 
     public void exit() {
         controller.exit();
