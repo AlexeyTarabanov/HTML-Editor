e9ec17ed013b5e94576f902e011fb51ad3026141
Alexey Tarabanov <tarabanov@bk.ru>
2021-03-19 14:16:08 +0300

    15/24

--- Controller.java
+++ Controller.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task32.task3209;
 
 import javax.swing.text.html.HTMLDocument;
+import javax.swing.text.html.HTMLEditorKit;
 import java.io.File;
 
 /**
@@ -9,56 +10,56 @@ import java.io.File;
  Шаг 1.
  1. Создал классы Controller и View.
  2. В классе Controller:
-  - добавил поля View view (отвечающие за представление), HTMLDocument document (модель)
-    и File currentFile (будет отвечать за файл, который сейчас открыт в нашем редакторе (текущий файл))
-  - добавил конструктор с полем View
-  - добавил метод main()
+ - добавил поля View view (отвечающие за представление), HTMLDocument document (модель)
+ и File currentFile (будет отвечать за файл, который сейчас открыт в нашем редакторе (текущий файл))
+ - добавил конструктор с полем View
+ - добавил метод main()
  3. Класс View:
-  - наследуется от JFrame и реализовывает интерфейс ActionListener
-  - имплементировал метод actionPerformed()
-  - добавил поле Controller controller, а также сеттер и геттер к нему.
+ - наследуется от JFrame и реализовывает интерфейс ActionListener
+ - имплементировал метод actionPerformed()
+ - добавил поле Controller controller, а также сеттер и геттер к нему.
 
  Шаг 2.
  1. В классы Controller и View добавил методы init(), пока без реализаций
  2. В классе Controller, в методе main:
-  - создал объект представления (объект класса View)
-  - создал контроллер (объект класса Controller)
-  - установил у представления контроллер (view.setController(controller))
-  - инициализировал представление (init)
-  - инициализировал контроллер (init)
+ - создал объект представления (объект класса View)
+ - создал контроллер (объект класса Controller)
+ - установил у представления контроллер (view.setController(controller))
+ - инициализировал представление (init)
+ - инициализировал контроллер (init)
  3. В классе Controller:
-  - создал и реализовал метод exit()
+ - создал и реализовал метод exit()
  4. В классе:
-  - оздал и реализовал метод exit()
+ - оздал и реализовал метод exit()
 
  Шаг 3.
  1. В классе View:
-  добавил поля:
-  - JTabbedPane tabbedPane - панель с двумя вкладками
-  - JTextPane htmlTextPane - компонент для визуального редактирования html. Он будет размещен на первой вкладке.
-  - JEditorPane plainTextPane - компонент для редактирования html в виде текста, он будет отображать код html (теги и их содержимое)
+ добавил поля:
+ - JTabbedPane tabbedPane - панель с двумя вкладками
+ - JTextPane htmlTextPane - компонент для визуального редактирования html. Он будет размещен на первой вкладке.
+ - JEditorPane plainTextPane - компонент для редактирования html в виде текста, он будет отображать код html (теги и их содержимое)
  2. Создал пакет listeners, в него добавил класс FrameListener.
-  Унаследовал его от WindowAdapter
+ Унаследовал его от WindowAdapter
  3. В классе FrameListener:
-  - добавил поле View view
-  - добавил конструктор с полем View
+ - добавил поле View view
+ - добавил конструктор с полем View
 
-  Шаг 4.
-  1. В классе View:
-  объявил методы:
-   - initMenuBar() - инициализация меню
-   - initEditor() - инициализация панелей редактора
-  2. Объявил и реализовал метод initGui() - инициализация графического интерфейса
-  3. Реализовал метод init()
+ Шаг 4.
+ 1. В классе View:
+ объявил методы:
+ - initMenuBar() - инициализация меню
+ - initEditor() - инициализация панелей редактора
+ 2. Объявил и реализовал метод initGui() - инициализация графического интерфейса
+ 3. Реализовал метод init()
 
  Шаг 5.
  1. В пакете listeners:
-  - объявил класс TabbedPaneChangeListener реализующий интерфейс ChangeListener
+ - объявил класс TabbedPaneChangeListener реализующий интерфейс ChangeListener
  2. В классе View:
-  - добавил метод selectedTabChanged()
+ - добавил метод selectedTabChanged()
  3. В классе TabbedPaneChangeListener:
-  - добавил поле View view и конструктор с этим полем
-  - реализовал метод stateChanged()
+ - добавил поле View view и конструктор с этим полем
+ - реализовал метод stateChanged()
  4. Создал класс ExceptionHandler, в нем создал и реализовал метод log(Exception e)
 
  Шаг 6.
@@ -68,82 +69,86 @@ import java.io.File;
  1. Создал класс MenuHelper
  (вспомогательный класс для инициализации и настройки меню)
  2. Объявил и реализовал методы:
-  - addMenuItem(JMenu parent, String text, ActionListener actionListener)
-  - addMenuItem(JMenu parent, Action action)
-  - addMenuItem(JMenu parent, String text, Action action)
+ - addMenuItem(JMenu parent, String text, ActionListener actionListener)
+ - addMenuItem(JMenu parent, Action action)
+ - addMenuItem(JMenu parent, String text, Action action)
  3. Создал методы:
-  - initHelpMenu(View view, JMenuBar menuBar) - инициализация меню помощи
-  - initFontMenu(View view, JMenuBar menuBar) - инициализация меню выбора шрифта
-  - initColorMenu(View view, JMenuBar menuBar) - инициализация меню выбора цвета
-  - initAlignMenu(View view, JMenuBar menuBar) - инициализация меню выравнивания
-  - initStyleMenu(View view, JMenuBar menuBar) - инициализация меню выбора стиля текста
-  - initEditMenu(View view, JMenuBar menuBar) - инициализация меню редактирования текста
-  - initFileMenu(View view, JMenuBar menuBar) - инициализация меню Файл
+ - initHelpMenu(View view, JMenuBar menuBar) - инициализация меню помощи
+ - initFontMenu(View view, JMenuBar menuBar) - инициализация меню выбора шрифта
+ - initColorMenu(View view, JMenuBar menuBar) - инициализация меню выбора цвета
+ - initAlignMenu(View view, JMenuBar menuBar) - инициализация меню выравнивания
+ - initStyleMenu(View view, JMenuBar menuBar) - инициализация меню выбора стиля текста
+ - initEditMenu(View view, JMenuBar menuBar) - инициализация меню редактирования текста
+ - initFileMenu(View view, JMenuBar menuBar) - инициализация меню Файл
 
  Шаг 8.
  1. Создал пакет actions,
-    в нем создал заглушки для следующих классов:
-  - класс отмены действия - UndoAction.
-  - класс возврата действия - RedoAction
-  - класс StrikeThroughAction - отвечает за стиль текста "Зачеркнутый"
-  - класс SubscriptAction - отвечает за стиль текста "Подстрочный знак"
-  - класс SuperscriptAction - отвечает за стиль "Надстрочный знак"
+ в нем создал заглушки для следующих классов:
+ - класс отмены действия - UndoAction.
+ - класс возврата действия - RedoAction
+ - класс StrikeThroughAction - отвечает за стиль текста "Зачеркнутый"
+ - класс SubscriptAction - отвечает за стиль текста "Подстрочный знак"
+ - класс SuperscriptAction - отвечает за стиль "Надстрочный знак"
  2. В пакете listeners, создал заглушки для следующих классов:
-  - класс UndoMenuListener
-  - класс TextEditMenuListener
+ - класс UndoMenuListener
+ - класс TextEditMenuListener
 
  Шаг 9.
  1. В классе View
-  - реализовал метод initMenuBar()
-  - создал конструктор, который устанавливает внешний вид и поведение (look and feel) нашего приложения такими же,
-  как это определено в системе
+ - реализовал метод initMenuBar()
+ - создал конструктор, который устанавливает внешний вид и поведение (look and feel) нашего приложения такими же,
+ как это определено в системе
 
  Шаг 10.
  1. В классе UndoMenuListener
-  - реализовал метод menuSelected(), который делает доступными или не доступными пункты меню undoMenuItem и redoMenuItem
+ - реализовал метод menuSelected(), который делает доступными или не доступными пункты меню undoMenuItem и redoMenuItem
  2. В классе View создал заглушки для следующих методов
-  - canUndo() - можем ли мы отменить действие
-  - canRedo() - можем повторить
+ - canUndo() - можем ли мы отменить действие
+ - canRedo() - можем повторить
 
  Шаг 11.
  1. В классе View:
-  - добавил поле UndoManager undoManager и проинициализировал его
-  - добавил поле UndoListener undoListener и проинициализировал его. Добавил геттер
-  Добавил и реализовал методы:
-  - void undo() - отменяет последнее действие
-  - void redo() - возвращает ранее отмененное действие
-  - void resetUndo() - сбрасывает все правки в менеджере undoManager
-  Реализовал методы:
-  - canUndo() и canRedo()
+ - добавил поле UndoManager undoManager и проинициализировал его
+ - добавил поле UndoListener undoListener и проинициализировал его. Добавил геттер
+ Добавил и реализовал методы:
+ - void undo() - отменяет последнее действие
+ - void redo() - возвращает ранее отмененное действие
+ - void resetUndo() - сбрасывает все правки в менеджере undoManager
+ Реализовал методы:
+ - canUndo() и canRedo()
  2. В пакете listeners:
-  - создал класс UndoListener реализующий интерфейс UndoableEditListener
-  (класс будет следить за правками, которые можно отменить или вернуть)
+ - создал класс UndoListener реализующий интерфейс UndoableEditListener
+ (класс будет следить за правками, которые можно отменить или вернуть)
  3. В классе UndoListener:
-  - добавил поле UndoManager undoManager и конструктор с этим полем
-  - реализовал метод undoableEditHappened(UndoableEditEvent e)
+ - добавил поле UndoManager undoManager и конструктор с этим полем
+ - реализовал метод undoableEditHappened(UndoableEditEvent e)
 
  Шаг 12.
  1. В классе RedoAction:
-  - реализовал метод actionPerformed(ActionEvent actionEvent)
+ - реализовал метод actionPerformed(ActionEvent actionEvent)
  2. В классе UndoAction:
-  - реализовал метод actionPerformed(ActionEvent e)
+ - реализовал метод actionPerformed(ActionEvent e)
  3. Реализовал классы SubscriptAction и SuperscriptAction
 
  Шаг 13.
  1. В пакете listeners:
-  - реализовал класс TextEditMenuListener
+ - реализовал класс TextEditMenuListener
  2. В классе View:
-  - добавил метод boolean isHtmlTabSelected()
+ - добавил метод boolean isHtmlTabSelected()
 
  Шаг 14.
  1. В классе View, объявил и реализовал методы:
-  - selectHtmlTab() - выбирает вкладку html и сбрасывает все правки
-  - update() - получает документ у контроллера и устанавливает его в панель редактирования htmlTextPane
-  - showAbout() - показывает диалоговое окно с информацией о программе
+ - selectHtmlTab() - выбирает вкладку html и сбрасывает все правки
+ - update() - получает документ у контроллера и устанавливает его в панель редактирования htmlTextPane
+ - showAbout() - показывает диалоговое окно с информацией о программе
  2. В классе Controller:
-  - добавил геттер для модели, в нашем случае это поле document
+ - добавил геттер для модели, в нашем случае это поле document
 
  Шаг 15.
+ 1. В классе Controller:
+  - объявил и реализовал метод resetDocument()
+
+ Шаг 16.
  1.
 
  */
@@ -166,7 +171,8 @@ public class Controller {
     }
 
     // инициализирует
-    public void init() {}
+    public void init() {
+    }
 
     public void exit() {
         // exit - используется для завершения программы.
@@ -174,6 +180,20 @@ public class Controller {
         // Обычно это 0, что говорит о том, что программа завершается без ошибок.
         // Любое другое значение говорит о том, что программа завершилась с ошибкой
         System.exit(0);
+    }
+
+    // сбрасывает текущий документ
+    public void resetDocument() {
+        // удаляет у текущего документа document слушателя правок, которые можно отменить/вернуть
+        if (document != null) {
+            document.removeUndoableEditListener(view.getUndoListener());
+        }
+        // создает новый документ по умолчанию и присваивать его полю document
+        document = (HTMLDocument) new HTMLEditorKit().createDefaultDocument();
+        // добавляет новому документу слушателя правок
+        document.addUndoableEditListener(view.getUndoListener());
+        // получает документ у контроллера и устанавливает его в панель редактирования htmlTextPane
+        view.update();
     }
 
     public static void main(String[] args) {
